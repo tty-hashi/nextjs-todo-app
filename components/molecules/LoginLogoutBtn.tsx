5d172e42ef735879e1b8877dac41bf64aa52de4c
tty-hash <hashi.ndig@gmail.com>
2022-10-14 08:52:34 +0900

    ヘッダーのログインボタンの出し分けをコンポーネント化した。

---
+++ components/molecules/LoginLogoutBtn.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { onAuthStateChanged } from "firebase/auth";
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useSetRecoilState } from 'recoil';
+
+import { auth, sinInWithGoogle } from '../../firebase/firebase-settings'
+import { userIdState } from '../../states/state';
+import Btn from '../atoms/Btn';
+
+
+const LoginLogoutBtn: React.FC = () => {
+  const setUserId = useSetRecoilState(userIdState)
+  // userの変更をfirebasehooksで検知してstateを書き換え
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setUserId(user.uid);
+    }
+  });
+
+  //firebasehooksでuserのログイン・ログアウト状態を管理
+  const [user] = useAuthState(auth);
+  //googleサインアウト
+  const singOutWithGoogle = () => {
+    auth.signOut();
+  }
+
+  return (
+    <>
+      {!user ? <Btn buttonText={'ログイン'} onClickHandler={sinInWithGoogle} /> :
+        <Btn buttonText={'ログアウト'} onClickHandler={singOutWithGoogle} />}
+    </>
+  )
+}
+
+export default LoginLogoutBtn
