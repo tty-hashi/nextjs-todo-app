b08b811c555fb5f5c918605f4ad77137aa1dc3f9
tty-hash <hashi.ndig@gmail.com>
2022-10-17 07:16:01 +0900

    タスクリスト表示させた。

--- components/molecules/TodoList.tsx
+++ components/molecules/TodoList.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useRecoilValue } from 'recoil';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const TodoList: React.FC = () => {
+import { taskItemState, userIdState } from '../../states/state';
+import { useFetchTodos } from '../hooks/useFetchTodos';
+import { faTrash, faFilePen } from '@fortawesome/free-solid-svg-icons'
+import { Box, Flex, ListItem, Select, Spacer, UnorderedList } from '@chakra-ui/react';
+
+const TodoList = () => {
+  const { fetchTodos } = useFetchTodos();
+  const taskItems = useRecoilValue(taskItemState)
+  const uid = useRecoilValue(userIdState)
+  //firebaseからtaskを取得して、stateを更新
+
+  useEffect(() => {
+    fetchTodos(uid);
+  }, [])
+  console.log(taskItems);
+
   return (
-    <div>TodoList</div>
+    <>
+      <UnorderedList>
+        {taskItems.map(todo => (
+          <ListItem display='flex' mb={4}>
+            <p>{todo.content}</p>
+            <Spacer />
+            <Flex alignItems='center'>
+              <Select w={'100px'} marginRight={4}>
+                <option value="nostarted">nostarted</option>
+                <option value="inprogress">inprogress</option>
+                <option value="done">done</option>
+              </Select>
+              <FontAwesomeIcon icon={faFilePen} style={{ marginRight: '10px' }} />
+              <FontAwesomeIcon icon={faTrash} />
+            </Flex>
+          </ListItem>
+        ))}
+      </UnorderedList>
+    </>
   )
 }
 
--- states/state.ts
+++ states/state.ts
@@ -3,10 +3,14 @@ import { atom } from 'recoil'
 export const userIdState = atom<string>({
   key: 'userIdState', //一意
   default: '', //初期値
-
 })
 
 export const todoInputState = atom<string>({
   key: 'todoInputState',
   default: '',
+})
+
+export const taskItemState = atom<Array<any>>({
+  key: 'taskItems',
+  default: [{ id: '', content: '', createdAt: '', status: 'noStarted' }]
 })
